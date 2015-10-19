2e15ad027578acbfb9d53aa117d3a503dd58b153
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2015-10-19 02:26:38 +0100

    directives

--- script.js
+++ script.js
@@ -6,23 +6,27 @@
 		
 		var onUserComplete = function(response){
 			s.user = response.data;
+			w.get(s.user.repos_url)
+			.then(onRepos, onError);
 		};
+
+		var onRepos = function(response){
+			s.repos = response.data;
+		}
 
 		var onError = function(reason){
 			s.error = "could not fetch the user";
 		};
 
-		w.get("https://api.github.com/users/MrHanachoo")
+		s.search = function(username){
+			w.get("https://api.github.com/users/" + username)
 		.then(onUserComplete, onError);
-		/*
-		var person = {
-			firstName: "Mohammed",
-			lastName: "Hannechi",
-			imageSrc: "file:///home/med/Desktop/me.jpg"
 		};
-		*/
-		s.message = "Hello Angular !";
-		//$scope.person =person;
+		
+		s.username = "angular";
+		s.message = "GitHub Viewer";
+		s.repoSortOrder = "-stargazers_count";
+
 	};
 
 	app.controller("MainController", ["$scope", "$http", MainController]);
