646da8d095a219b8527e859fab25c5662700ed2a
Mohammed Hannechi <mohammed.hannechi@gmail.com>
2015-11-22 12:08:59 +0100

    ng-route

--- MainController.js
+++ MainController.js
@@ -1,25 +1,9 @@
 (function(){
 
-	var app = angular.module("githubViewer", []);
+	var app = angular.module("githubViewer");
 
 	var MainController = function(
-			$scope, github, $interval,
-			$log, $anchorScroll, $location){
-		
-		var onUserComplete = function(data){
-			$scope.user = data;
-			github.getRepos($scope.user).then(onRepos, onError);
-		};
-
-		var onRepos = function(data){
-			$scope.repos = data;
-			$location.hash("userDetails");
-			$anchorScroll();
-		}
-
-		var onError = function(reason){
-			$scope.error = "could not fetch the user";
-		};
+			$scope, $interval, $location){
 
 		var decrementCountdown = function(){
 			$scope.countdown -= 1;
@@ -35,17 +19,15 @@
 		};
 
 		$scope.search = function(username){
-			$log.info("Searching for "+ username);
-			github.getUser(username).then(onUserComplete, onError);
+
 			if(countdownInterval){
 				$interval.cancel(countdownInterval);
 				$scope.countdown = null;
 			}
+			$location.path("/user/"+ username);
 		};
 		
 		$scope.username = "angular";
-		$scope.message = "GitHub Viewer";
-		$scope.repoSortOrder = "-stargazers_count";
 		$scope.countdown = 5;
 		startCountdown();
 	};
--- github.js
+++ github.js
@@ -19,9 +19,26 @@
                     return response.data;
                 });
         };
+
+        var getRepoDetails = function (username, reponame) {
+            var repo;
+            var repoUrl = "https://api.github.com/repos/"+ username+ "/"+ reponame;
+
+            return $http.get(repoUrl)
+                .then(function (response) {
+                    repo = response.data;
+                    return $http.get(repoUrl + "/contributors");
+                })
+                .then(function (response) {
+                    repo.collaborators = response.data;
+                    return repo;
+                });
+        };
+
         return{
             getUser: getUser,
-            getRepos: getRepos
+            getRepos: getRepos,
+            getRepoDetails: getRepoDetails
 
         };
     };
